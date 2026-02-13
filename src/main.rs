a4e5ed2bc48be9620a8a7af5acf5b4abaad81c0c
0xcurat0r <mineofshadow@gmail.com>
2026-02-13 16:15:43 +0300

    vga buffer hope it works

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,13 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
-    loop{}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1 ) = 0xb;
+        }
+    }
+    loop {}
 }
