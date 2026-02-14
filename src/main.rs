fff1e250e969ca6bc2166114d5f1392b61746e5c
0xcurat0r <mineofshadow@gmail.com>
2026-02-14 19:15:50 +0300

    comment

--- src/main.rs
+++ src/main.rs
@@ -24,3 +24,5 @@ pub extern "C" fn _start() -> !{
     } // prints hello world! in BLUE
     loop {}
 }
+// qemu-system-x86_64 -drive format=raw,file=target/x86_64-mini-os/debug/bootimage-mini-os.bin 
+
