52585e1b35d50000f92cb3a581554d46cf686c81
0xcurat0r <mineofshadow@gmail.com>
2026-02-16 19:55:15 +0300

    pub fn

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -49,5 +49,25 @@ pub struct Writer {
 }
 
 impl Writer{
-    
+    pub fn write_byte(&mut self, byte: u8){
+        match byte{
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH{
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar { ascii_character: byte, color_code, };
+                self.column_position += 1;
+            }
+
+        }
+    }
+    fn new_line(&mut self){
+        //to doo
+    }
 }
