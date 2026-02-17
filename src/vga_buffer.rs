6afdaa1577fe9d54e07d2d883fb9a565ec5d3a81
0xcurat0r <mineofshadow@gmail.com>
2026-02-17 03:24:25 +0300

    does this still compile?

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -37,9 +37,10 @@ struct ScreenChar{
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
+use volatile::Volatile;
 #[repr(transparent)]
 struct Buffer{
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -61,7 +62,7 @@ impl Writer{
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar { ascii_character: byte, color_code, };
+                self.buffer.chars[row][col].write(ScreenChar { ascii_character: byte, color_code, });
                 self.column_position += 1;
             }
 
