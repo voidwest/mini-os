092671ba6e054b5343f16ba5a0fa1b659e8e46e2
0xcurat0r <mineofshadow@gmail.com>
2026-02-19 02:25:13 +0300

    commit

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -78,7 +78,12 @@ impl Writer{
         }
     }
     fn new_line(&mut self){
-        
+        for row in 1..BUFFER_HEIGHT{
+            for col in 0..BUFFER_WIDTH{
+                let character = self.buffer.chars[row][col].read();
+                
+            }
+        }
     }
 
     fn clear_line(&mut self, row: usize){
