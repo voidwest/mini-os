93710f7fcc8230992dffd343b89cac91a5fda47e
0xcurat0r <mineofshadow@gmail.com>
2026-02-21 04:52:13 +0300

    rewrote strings

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> !{
     use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("hi").unwrap();
+    vga_buffer::WRITER.lock().write_str("whats up \n").unwrap();
     write!(vga_buffer::WRITER.lock(), "nums {}{}", 42, 1.2).unwrap();
     loop {}
 }
