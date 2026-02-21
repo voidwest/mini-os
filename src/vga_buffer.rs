83a80b4b9c90bc8b8327abbff2f443a6b36a5d64
0xcurat0r <mineofshadow@gmail.com>
2026-02-21 05:00:43 +0300

    println final

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -127,6 +127,6 @@ macro_rules! print {
 #[macro_export]
 macro_rules! println {
     () => ($crate::print!("\n"));
-    ($(arg::tt)*) => ($crate::print!(format_args!($($arg)*)));
+    ($(arg::tt)*) => ($crate::print!("{}\n" ,format_args!($($arg)*)));
         
 }
