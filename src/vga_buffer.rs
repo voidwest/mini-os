6fb6d7c1ce7b2e5eb502bbb6eb4b6206a1bde6ff
0xcurat0r <mineofshadow@gmail.com>
2026-02-21 04:59:32 +0300

    very terrifying typing this

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -126,6 +126,7 @@ macro_rules! print {
 
 #[macro_export]
 macro_rules! println {
-    () => 
+    () => ($crate::print!("\n"));
+    ($(arg::tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
         
 }
