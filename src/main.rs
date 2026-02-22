0fb57887e9ac19377d6672de525314c6e24515a1
0xcurat0r <mineofshadow@gmail.com>
2026-02-22 23:35:28 +0300

    setting up testing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-
+#![test_runner(crate::test_runner)]
 use core::{fmt::write, panic::PanicInfo};
 mod vga_buffer;
 
@@ -19,3 +19,4 @@ pub extern "C" fn _start() -> !{
         loop {}
 }
 
+#[cfg(test)]
