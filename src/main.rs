1e0bb07784caec60db1be0d0cd64d19fd3e3b565
0xcurat0r <mineofshadow@gmail.com>
2026-02-24 04:55:41 +0300

    function for exiting

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,15 @@ pub enum QemuExitCode {
     Failure = 0x11,
 }
 
+pub fn exit_qemu(exit_code: QemuExitCode){
+    use x86_64::instructions::port::Port;
+
+    unsafe{
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]){
     println!("running {} tests", tests.len());
