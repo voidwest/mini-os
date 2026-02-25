1c606fb72612c49bf969372bd12a5f4dae0d4f3c
0xcurat0r <mineofshadow@gmail.com>
2026-02-26 01:25:43 +0300

    no errors so far

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -136,3 +136,15 @@ pub fn _print(args: fmt::Arguments){
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple(){
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_alot(){
+    for _ in 0..200{
+        println!("test_println_alot outpit");
+    }
+}
