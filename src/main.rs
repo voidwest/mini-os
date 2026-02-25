2c7e4e38aae22e6eba826cfeeb649213cd924d31
0xcurat0r <mineofshadow@gmail.com>
2026-02-25 06:03:56 +0300

    ok it compiles still

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ pub fn exit_qemu(exit_code: QemuExitCode){
 
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]){
-    println!("running {} tests", tests.len());
+    serial_println!("running {} tests", tests.len());
     for test in tests{
         test();
     }
