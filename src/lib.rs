193940a71097cc2d08cf4796d029bd4fd85f172b
0xcurat0r <mineofshadow@gmail.com>
2026-02-28 01:29:53 +0300

    weird but alr - fixed import

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 #![cfg_attr(test, no_main)]
-#![feature((custom_test_frameworks))]
+#![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -49,4 +49,20 @@ pub extern "C" fn _start() -> !{
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
     test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
