5f8695dcc0648ccddd5f7e8eb63cbdddbac90a7f
0xcurat0r <mineofshadow@gmail.com>
2026-02-28 01:16:16 +0300

    reimports

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 #![no_std]
+
+#![cfg_attr(test, no_main)]
+#![feature((custom_test_frameworks))]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
