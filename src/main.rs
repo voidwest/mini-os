105be9ad0d9e1511251a0c1c4317df965a9635bd
0xcurat0r <mineofshadow@gmail.com>
2026-02-28 01:33:53 +0300

    trimmed down more

--- src/main.rs
+++ src/main.rs
@@ -25,24 +25,6 @@ pub extern "C" fn _start() -> !{
         loop {}
 }
 
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failure = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode){
-    use x86_64::instructions::port::Port;
-
-    unsafe{
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> !{
