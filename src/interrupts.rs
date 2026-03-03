8e76db546e8528ec6121e885827a2e7d7bad5467
0xcurat0r <mineofshadow@gmail.com>
2026-03-03 07:42:28 +0300

    ok that does not compile.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,14 +1,10 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
-pub fn init_idt(){
-    unsafe{
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load();
-    }
-}
+
+
+
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame){
 println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
