2b7fba1d33a6fdfa20939300388d2a77a0c8adfd
0xcurat0r <mineofshadow@gmail.com>
2026-03-03 07:41:33 +0300

    forgot semicolon

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,7 +6,7 @@ static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 pub fn init_idt(){
     unsafe{
         IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load()
+        IDT.load();
     }
 }
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame){
