aec297e714febe74e0d87a2cf4a4f466566bed9f
0xcurat0r <mineofshadow@gmail.com>
2026-03-03 07:49:16 +0300

    should compile

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,12 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> !{
     println!("Hello World{}", "!");
 
+    mini_os::init();
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
+    println!("didn't crash yet");
         loop {}
 }
 
