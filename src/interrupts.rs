d706ff6e81929f9c17e34f4557b3d3d4e2a386da
0xcurat0r <mineofshadow@gmail.com>
2026-03-03 07:46:44 +0300

    works, used lazy static to get over evaluating static at compile time

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -2,7 +2,17 @@ use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 use lazy_static::lazy_static;
 
+lazy_static!{
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt   
+    };
+}
 
+pub fn init_idt(){
+    IDT.load();
+}
 
 
 
