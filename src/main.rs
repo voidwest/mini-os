27de93a1766518308e7a8792fd6cd18fe9ce9980
0xcurat0r <mineofshadow@gmail.com>
2026-03-04 00:57:51 +0300

    triple fault

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,10 @@ pub extern "C" fn _start() -> !{
 
     mini_os::init();
     x86_64::instructions::interrupts::int3();
+    fn stack_overflow(){
+        stack_overflow();
+    }
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
