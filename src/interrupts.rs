d7e4cc25f3f945ef42035f8780b96be787f9a234
0xcurat0r <mineofshadow@gmail.com>
2026-03-05 01:10:49 +0300

    more imports and barebones page fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -32,6 +32,13 @@ extern "x86-interrupt" fn double_fault_handler(
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+}
+
 #[test_case]
 fn test_breakpoint_exception() {
     x86_64::instructions::interrupts::int3();
