504113ccf68142555f1188ab1cbd32b8258d863e
0xcurat0r <mineofshadow@gmail.com>
2026-03-06 01:27:39 +0300

    first new argument in _start

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     mini_os::init();
