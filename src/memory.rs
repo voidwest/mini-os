bc37865645f8fcca8448991392a74272634a6862
0xcurat0r <mineofshadow@gmail.com>
2026-03-07 09:36:36 +0300

    barebones func, rest tmrw

--- src/memory.rs
+++ src/memory.rs
@@ -16,3 +16,7 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
 
     unsafe { &mut *page_table_ptr }
 }
+
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
