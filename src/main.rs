884c405cb22d047622d02101eaee67d2cd9a686b
0xcurat0r <mineofshadow@gmail.com>
2026-03-07 09:32:40 +0300

    it works

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,75 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         use x86_64::structures::paging::PageTable;
         if !entry.is_unused() {
             println!("L4 entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("L3 entry {}: {:?}", i, entry);
+                }// main.rs
+                #![no_std]
+                #![no_main]
+                #![feature(custom_test_frameworks)]
+                #![test_runner(mini_os::test_runner)]
+                #![reexport_test_harness_main = "test_main"]
+                
+                use bootloader::{BootInfo, entry_point};
+                use core::panic::PanicInfo;
+                use mini_os::println;
+                
+                // called on panic
+                #[cfg(not(test))]
+                #[panic_handler]
+                fn panic(info: &PanicInfo) -> ! {
+                    println!("{}", info);
+                    loop {}
+                }
+                entry_point!(kernel_main);
+                
+                fn kernel_main(boot_info: &'static BootInfo) -> ! {
+                    use mini_os::memory::active_level_4_table;
+                    use x86_64::VirtAddr;
+                
+                    mini_os::init();
+                    println!("Hello World{}", "!");
+                
+                    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+                    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+                
+                    for (i, entry) in l4_table.iter().enumerate() {
+                        use x86_64::structures::paging::PageTable;
+                        if !entry.is_unused() {
+                            println!("L4 entry {}: {:?}", i, entry);
+                        }
+                        let phys = entry.frame().unwrap().start_address();
+                        let virt = phys.as_u64() + boot_info.physical_memory_offset;
+                        let ptr = VirtAddr::new(virt).as_mut_ptr();
+                        let l3_table: &PageTable = unsafe { &*ptr };
+                
+                        for (i, entry) in l3_table.iter().enumerate() {
+                            if !entry.is_unused() {
+                                println!("L3 entry {}: {:?}", i, entry);
+                            }
+                        }
+                    }
+                
+                    #[cfg(test)]
+                    test_main();
+                    println!("didn't crash yet");
+                    mini_os::hlt_loop();
+                }
+                
+                #[cfg(test)]
+                #[panic_handler]
+                fn panic(info: &PanicInfo) -> ! {
+                    mini_os::test_panic_handler(info)
+                }
+
+            }
         }
     }
 
