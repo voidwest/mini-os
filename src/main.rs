f0497cc76a32a1b8841e92c2bab017b5c31a131c
0xcurat0r <mineofshadow@gmail.com>
2026-03-08 02:01:20 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn panic(info: &PanicInfo) -> ! {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use mini_os::memory::BootInfoFrameAllocator;
     use x86_64::structures::paging::Page;
     use x86_64::{VirtAddr, structures::paging::Translate};
 
@@ -26,7 +27,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     let page = Page::containing_address(VirtAddr::new(0));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
