83ff27b4cea48ee81576c9a2e4d32061885b4f85
0xcurat0r <mineofshadow@gmail.com>
2026-03-08 01:07:49 +0300

    new test: addr translation

--- src/main.rs
+++ src/main.rs
@@ -19,32 +19,27 @@ fn panic(info: &PanicInfo) -> ! {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use mini_os::memory::active_level_4_table;
+    use mini_os::memory::translate_addr;
     use x86_64::VirtAddr;
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+
+    let addresses = [
+        0xb000,
+        0x21008,
+        0x0100_0020_0a10,
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     mini_os::init();
     println!("Hello World{}", "!");
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-
-    for (i, entry) in l4_table.iter().enumerate() {
-        use x86_64::structures::paging::PageTable;
-        if !entry.is_unused() {
-            println!("L4 entry {}: {:?}", i, entry);
-
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 entry {}: {:?}", i, entry);
-                }
-            }
-        }
-    }
     #[cfg(test)]
     test_main();
     println!("didn't crash yet");
