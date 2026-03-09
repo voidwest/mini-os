8557bc8b55a24e5e7916adedc219dd346327be32
0xcurat0r <mineofshadow@gmail.com>
2026-03-10 01:25:08 +0300

    Update heap_allocation.rs

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -12,7 +12,18 @@ use core::panic::PanicInfo;
 entry_point!(main);
 
 fn main(boot_info: &'static BootInfo) -> ! {
-    unimplemented!();
+    use mini_os::allocator;
+    use mini_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    mini_os::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    test_main();
+    loop {}
 }
 
 #[panic_handler]
