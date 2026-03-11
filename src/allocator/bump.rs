54cb175c21043b1e924dd415822446c5ee41336d
0xcurat0r <mineofshadow@gmail.com>
2026-03-11 03:01:30 +0300

    init function

--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -4,3 +4,20 @@ pub struct BumpAllocator {
     next: usize,
     allocations: usize,
 }
+
+impl BumpAllocator {
+    pub const fn new() -> self {
+        BumpAllocator {
+            heap_start: 0,
+            heap_end: 0,
+            next: 0,
+            allocations: 0,
+        }
+    }
+
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.heap_start = heap_start;
+        self.heap_end = heap_start + heap_size;
+        self.next = heap_start;
+    }
+}
