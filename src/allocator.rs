529969baba4480fa1a9413a1bcfa19aacfeff0da
0xcurat0r <mineofshadow@gmail.com>
2026-03-11 03:11:09 +0300

    func

--- src/allocator.rs
+++ src/allocator.rs
@@ -54,3 +54,11 @@ pub fn init_heap(
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
 }
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+}
