3f8de739cd20da850c078e1330c64b25daf22ed3
0xcurat0r <mineofshadow@gmail.com>
2026-03-13 04:48:07 +0300

    Update allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -12,17 +12,7 @@ use x86_64::{
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; //100 kib
 
-pub struct Dummy;
 use bump::BumpAllocator;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
-    }
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("deakkic should never be called");
-    }
-}
 
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
