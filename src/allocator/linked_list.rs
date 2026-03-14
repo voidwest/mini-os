70a8381debc330803e0085cff1919f68a844e678
0xcurat0r <mineofshadow@gmail.com>
2026-03-14 04:14:56 +0300

    Update linked_list.rs

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -7,6 +7,8 @@ impl ListNode {
     const fn new(size: usize) -> Self {
         ListNode { size, next: None }
     }
-    
-    
+
+    fn start_addr(&self) -> usize {
+        self as *const Self as usize
+    }
 }
