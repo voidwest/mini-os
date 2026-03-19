75ee03ba4f4a0a052c6184425563e795a8599f25
0xcurat0r <mineofshadow@gmail.com>
2026-03-20 02:45:11 +0300

    Update fixed_size_block.rs

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -2,4 +2,4 @@ struct ListNode {
     next: Option<&'static mut ListNode>,
 }
 
-const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256];
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
