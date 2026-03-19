ec7acb91d12409cc42a587863803b170967db87a
0xcurat0r <mineofshadow@gmail.com>
2026-03-20 02:47:20 +0300

    Update fixed_size_block.rs

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -3,3 +3,14 @@ struct ListNode {
 }
 
 const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+
+pub struct FixedSizeBlockAllocator {
+    pub const fn new() -> Self{
+    const EMPTY: Option<&'static mut ListNode> = None;
+
+    FixedSizeBlockAllocator{
+        list_heads: [EMPTY, BLOCK_SIZES.len()],
+        fallback_allocator: linked_list_allocator::Heap::empty(),
+    }
+    }
+}
