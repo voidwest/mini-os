0ebd30718a3ce79bb85fa6fbff685497d67b6d67
0xcurat0r <mineofshadow@gmail.com>
2026-03-20 02:51:20 +0300

    Update fixed_size_block.rs

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -5,12 +5,21 @@ struct ListNode {
 const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
 
 pub struct FixedSizeBlockAllocator {
-    pub const fn new() -> Self{
-    const EMPTY: Option<&'static mut ListNode> = None;
+    list_heads: [Option<&'static mut ListNode>; BLOCK_SIZES.len()],
+    fallback_allocator: linked_list_allocator::Heap,
+}
 
-    FixedSizeBlockAllocator{
-        list_heads: [EMPTY, BLOCK_SIZES.len()],
-        fallback_allocator: linked_list_allocator::Heap::empty(),
+impl FixedSizeBlockAllocator {
+    pub const fn new() -> Self {
+        const EMPTY: Option<&'static mut ListNode> = None;
+
+        FixedSizeBlockAllocator {
+            list_heads: [EMPTY, BLOCK_SIZES.len()],
+            fallback_allocator: linked_list_allocator::Heap::empty(),
+        }
     }
+
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        unsafe { self.fallback_allocator.init(heap_start, heap_size) }
     }
 }
