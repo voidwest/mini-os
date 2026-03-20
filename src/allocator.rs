c45497c39bb3942dc2983a9277e3bc4c3a9f118a
0xcurat0r <mineofshadow@gmail.com>
2026-03-20 22:25:03 +0300

    Update allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -12,6 +12,7 @@ pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; //100 kib
 
 use bump::BumpAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 use linked_list::LinkedListAllocator;
 
 #[global_allocator]
