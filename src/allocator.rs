fb58047f90db9ec64837d7d9c91e535c7bccdbc2
0xcurat0r <mineofshadow@gmail.com>
2026-03-21 01:57:54 +0300

    Update allocator.rs

--- src/allocator.rs
+++ src/allocator.rs
@@ -11,9 +11,7 @@ use x86_64::{
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; //100 kib
 
-use bump::BumpAllocator;
 use fixed_size_block::FixedSizeBlockAllocator;
-use linked_list::LinkedListAllocator;
 
 #[global_allocator]
 static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
