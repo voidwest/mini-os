963b738c731da36278c92781050a2b240b3b9966
0xcurat0r <mineofshadow@gmail.com>
2026-03-21 01:56:36 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,7 @@ pub fn init() {
     gdt::init();
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub fn hlt_loop() -> ! {
