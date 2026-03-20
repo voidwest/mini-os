de1a6cea1f2d646650a60356cffd602760d3f9fd
0xcurat0r <mineofshadow@gmail.com>
2026-03-21 01:59:06 +0300

    Update interrupts.rs

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -57,3 +57,9 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub static PICS: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
