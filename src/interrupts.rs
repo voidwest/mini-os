e9b08ee93d3c17462a39fac831daf45dd1f86139
0xcurat0r <mineofshadow@gmail.com>
2026-03-22 00:06:34 +0300

    67

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -87,7 +87,7 @@ lazy_static! {
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
+    print!("67 ");
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
