969375d4f9b5bbfaa628a0a96f613b2bee115f7e
0xcurat0r <mineofshadow@gmail.com>
2026-03-21 23:37:26 +0300

    cargo fixes

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -88,10 +88,7 @@ impl LinkedListAllocator {
 }
 
 use super::Locked;
-use alloc::{
-    alloc::{GlobalAlloc, Layout},
-    string::ToString,
-};
+use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr;
 
 unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
