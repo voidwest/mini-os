e28b79e79fd4783cef204b9a80d0635e7e74b1f5
voidwest <mineofshadow@gmail.com>
2026-05-06 21:17:45 +0300

    Update gdt.rs

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,11 +1,12 @@
-use x86_64::VirtAddr;
-use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
+use x86_64::VirtAddr;
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
+use x86_64::structures::tss::TaskStateSegment;
 
+/// Index in the TSS interrupt stack table for the double-fault handler.
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
-lazy_static!{
+lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
@@ -16,31 +17,58 @@ lazy_static!{
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
+        tss.privilege_stack_table[0] = {
+            const KERNEL_STACK_SIZE: usize = 4096 * 5;
+            static mut KERNEL_STACK: [u8; KERNEL_STACK_SIZE] = [0; KERNEL_STACK_SIZE];
+
+            let stack_start = VirtAddr::from_ptr(&raw const KERNEL_STACK);
+            let stack_end = stack_start + KERNEL_STACK_SIZE;
+            stack_end
+        };
         tss
     };
 }
 
-lazy_static!{
+lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-       let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (gdt, Selectors {code_selector, tss_selector})
+        let user_data_selector = gdt.add_entry(Descriptor::user_data_segment());
+        let user_code_selector = gdt.add_entry(Descriptor::user_code_segment());
+        (gdt, Selectors { code_selector, tss_selector, user_data_selector, user_code_selector })
     };
 }
 
-struct Selectors {
+pub struct Selectors {
     code_selector: SegmentSelector,
     tss_selector: SegmentSelector,
+    user_data_selector: SegmentSelector,
+    user_code_selector: SegmentSelector,
 }
 
-pub fn init(){
-    use x86_64::instructions::tables::load_tss;
+impl Selectors {
+    pub fn user_data_selector(&self) -> SegmentSelector {
+        self.user_data_selector
+    }
+    pub fn user_code_selector(&self) -> SegmentSelector {
+        self.user_code_selector
+    }
+}
+
+/// Load the GDT, reload the code segment selector, and load the TSS.
+pub fn init() {
     use x86_64::instructions::segmentation::{CS, Segment};
+    use x86_64::instructions::tables::load_tss;
 
     GDT.0.load();
-    unsafe{
+    unsafe {
         CS::set_reg(GDT.1.code_selector);
         load_tss(GDT.1.tss_selector);
     }
+}
+
+/// Return the selectors for user-mode code and data segments.
+pub fn user_selectors() -> (SegmentSelector, SegmentSelector) {
+    (GDT.1.user_code_selector(), GDT.1.user_data_selector())
 }
