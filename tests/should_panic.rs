e4dd71db666d716263c6b88cf9db3bcef7582c81
voidwest <mineofshadow@gmail.com>
2026-05-06 21:18:37 +0300

    Update should_panic.rs

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -4,21 +4,21 @@
 use core::panic::PanicInfo;
 use mini_os::{QemuExitCode, exit_qemu, serial_println};
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> !{
+pub extern "C" fn _start() -> ! {
     should_fail();
     serial_println!("[did not panic]");
     exit_qemu(QemuExitCode::Failed);
-    loop{}
+    loop {}
 }
 
-fn should_fail(){
+fn should_fail() {
     serial_println!("should_panic should fail....\t");
     assert_eq!(0, 1);
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
-    loop{}
+    loop {}
 }
